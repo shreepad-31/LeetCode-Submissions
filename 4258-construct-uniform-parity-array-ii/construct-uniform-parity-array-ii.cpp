e1bee19331bf8f96609460242d9b163bfc480490
Shreepad Jagtap <shreedeshmukh3105@gmail.com>
2026-09-03 18:19:39 +0530

    Time: 2 ms (74.61%) | Memory: 165.9 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -3,12 +3,13 @@ public:
     bool uniformArray(vector<int>& nums1) {
         int LowestOdd = INT_MAX, LowestEven = INT_MAX;
         for(int i = 0; i < nums1.size(); i++){
-            if((nums1[i] % 2) && nums1[i] < LowestOdd) LowestOdd = nums1[i];
+
+            if((nums1[i] % 2)) LowestOdd = min(LowestOdd, nums1[i]);
             else LowestEven = min(LowestEven, nums1[i]);
         }
-        if(LowestEven == INT_MAX) return true;
-        else if(LowestOdd == INT_MAX) return true;
-        else if(LowestEven - LowestOdd > 0) return true;
-        return false;
+
+        if(LowestEven == INT_MAX || LowestOdd == INT_MAX) return true;
+
+        return LowestEven > LowestOdd;
     }
 };
